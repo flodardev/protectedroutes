dcfeb0489815ddafe0d28f67848c83c3f5f5aefb
flodardev <63950579+flodardev@users.noreply.github.com>
2020-07-05 22:20:56 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
+import Settings from "./components/Settings";
+import ProtectedRoute from "./components/ProtectedRoute";
 
-function App() {
+import "./styles.css";
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <ProtectedRoute exact={true} path="/" component={Dashboard} />
+          <ProtectedRoute path="/settings" component={Settings} />
+          <ProtectedRoute component={Dashboard} />
+        </Switch>
+      </Router>
     </div>
   );
 }
-
-export default App;
